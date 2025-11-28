8dc5ebbb64b27f191f17f626ef1f4c97ba4b4d81
Muhammad Abu Bakar <abubakar1house@gmail.com>
2025-11-28 15:31:42 +0500

    Clean up firebaseConfig.js by removing unused code
    Removed unused imports and Firebase configuration details.

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,24 +1,5 @@
-// Import the functions you need from the SDKs you need
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import { initializeApp } from "firebase/app";
-import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
+//Import Firebase Here
 
-import { getFirestore } from "firebase/firestore";
- 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC4RPuHyoYjghLAhhKknLt4KtHvv5MEcNM",
-  authDomain: "netflix-clone-adb5e.firebaseapp.com",
-  projectId: "netflix-clone-adb5e",
-  storageBucket: "netflix-clone-adb5e.firebasestorage.app",
-  messagingSenderId: "869584238707",
-  appId: "1:869584238707:web:d742cc40ebe888518d1206",
-  measurementId: "G-LZPKE05JGB"
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
